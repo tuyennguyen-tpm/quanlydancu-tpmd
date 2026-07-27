6b5eaefc83368062c226285e51ea0a9dab357e4a
tiendungnamsamson-cyber <tiendungnamsamson@gmail.com>
2026-07-27 12:59:02 +0700

    update code

--- src/utils/financialEngine.ts
+++ src/utils/financialEngine.ts
@@ -585,7 +585,7 @@ export function generateUnifiedHouseholdReceiptHtml(
           </tr>
           <tr>
             <td class="receipt-info-label" style="font-weight: bold; text-align: left;">Lý do nộp:</td>
-            <td style="text-align: left;">Thu tổng hợp các khoản đóng góp tự nguyện (TDP + UBND) năm ${new Date().getFullYear()}</td>
+            <td style="text-align: left;">Thu tổng hợp các khoản đóng góp tự nguyện (TDP + PHƯỜNGDate().getFullYear()}</td>
           </tr>
         </table>
 
