925175a24617d10b7278ca0d2bcaa5fb3570cd0c
tiendungnamsamson-cyber <tiendungnamsamson@gmail.com>
2026-08-08 17:02:38 +0700

    update code

---
+++ CHAY_PHAN_MEM_OFFLINE.bat
@@ -0,0 +1,4 @@
+@echo off
+title Mo Phan Mem Quan Ly TDP Quang Giao (Offline)
+echo Dang khoi chay Phan mem Quan ly Dan cu TDP Quang Giao (Offline)...
+start "" "%~dp0dist-electron\win-unpacked\QuanLyDanCuTDPQuangGiao.exe"
