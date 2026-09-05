174b6d248f04e01dfaaacf63d267a47770698427
tiendungnamsamson-cyber <tiendungnamsamson@gmail.com>
2026-09-05 13:56:46 +0700

    fix offline bat executable name to QuanLyDanCuTDPQuangGiao.exe

--- CHAY_PHAN_MEM_OFFLINE.bat
+++ CHAY_PHAN_MEM_OFFLINE.bat
@@ -1,4 +1,23 @@
 @echo off
 title Mo Phan Mem Quan Ly TDP Quang Giao (Offline)
+cd /d "%~dp0"
+
 echo Dang khoi chay Phan mem Quan ly Dan cu TDP Quang Giao (Offline)...
-start "" "%~dp0dist-electron\win-unpacked\electron.exe"
+
+if exist "dist-electron\win-unpacked\QuanLyDanCuTDPQuangGiao.exe" (
+    start "" "dist-electron\win-unpacked\QuanLyDanCuTDPQuangGiao.exe"
+    exit /b
+)
+
+if exist "QuanLyDanCuTDPQuangGiao_CHAY_NGAY.exe" (
+    start "" "QuanLyDanCuTDPQuangGiao_CHAY_NGAY.exe"
+    exit /b
+)
+
+if exist "dist-electron\QuanLyDanCuTDPQuangGiao_CHAY_NGAY.exe" (
+    start "" "dist-electron\QuanLyDanCuTDPQuangGiao_CHAY_NGAY.exe"
+    exit /b
+)
+
+echo [LOI] Khong tim thay file chay ung dung (.exe).
+pause
